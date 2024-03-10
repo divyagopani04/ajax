e89a1b70089892dd36c929cd967c98f5d72dcf7d
divyagopani04 <foritfile@gmail.com>
2024-03-10 21:35:25 +0530

    api

---
+++ crudapp/models.py
@@ -0,0 +1,12 @@
+from django.forms import ModelForm
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+class ProductForm(ModelForm):
+    class Meta:
+        model = Product
+        fields = ['name', 'price']
---
+++ crudapp/views.py
@@ -0,0 +1,38 @@
+# crudapp/views.py
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import JsonResponse
+from .models import Product
+from .forms import ProductForm
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'crudapp/product_list.html', {'products': products})
+
+def product_create(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')
+    else:
+        form = ProductForm()
+    return render(request, 'crudapp/product_form.html', {'form': form})
+
+def product_update(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')
+    else:
+        form = ProductForm(instance=product)
+    return render(request, 'crudapp/product_form.html', {'form': form})
+
+def product_delete(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'POST':
+        product.delete()
+        return redirect('product_list')
+    return render(request, 'crudapp/product_confirm_delete.html', {'product': product})
+
